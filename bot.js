ef4f41ecc155b88caaf5afe5efd20c11a89899c6
Glitch (hello-express) <none>
2021-02-28 17:08:22 +0000

    😇🍙 Checkpoint ./bot.js:25621915/2559 ./LICENSE.md:25621915/1104 ./package.json:25621915/880 ./CODE_OF_CONDUCT.md:25621915/6759 ./oku.md:25621915/20576

--- bot.js
+++ bot.js
@@ -13,8 +13,8 @@ const prefix = '.'
 setInterval(() => {
 const Linkler = db.get('Linkler')
 if(!Linkler) return;
-const Aventadoria = Linkler.map(Revenge => Revenge.url)
-Aventadoria.forEach(Link => {
+const De = Linkler.map(Revenge => Revenge.url)
+De.forEach(Link => {
 try {
 fetch(Link)
 } catch(e) {
@@ -40,126 +40,94 @@ client.on('message', async message => {
     const Revenge = new Discord.MessageEmbed()
     .setColor('RED')
     .setDescription(`
-    **==================================**
+    
     **Link Sistemde Zaten Bulunuyor. ❌** 
-    ==================================
+
     `)
     .setTimestamp()
     .setThumbnail(message.author.avatarURL)
     if(db.get('Linkler').map(Revenge => Revenge.url).includes(Link)) return message.channel.send(Revenge)
-    const Emrecan = new Discord.RichEmbed()
+    const success = new Discord.MessageEmbed()
     .setColor('GREEN')
     .setThumbnail(message.author.avatarURL)
     .setDescription(`
-    **==================================**
+    
     **Yazdığınız URL Başarıyla Eklendi. ✅**
     `)
-    .addField(prefix+'linkler','Komutunu Kullanarak Ekledigin Linklere Erisebilirsin')
-    .setTimestamp()
-    message.channel.send(Emrecan)
-    db.push('Linkler', { url: Link, owner: message.author.id, owner2: message.author.tag})
-    db.add(`Sahiplik_${message.author.id}`,1)
-    db.push(`Projesi_${message.author.id}`,Link)
+    .addField(prefix+'linkler','Komutunu Kullanarak Ekledigin Linklere Erisebilirsin')//PARADOX-DEVELOPMENT
+    .setTimestamp()//PARADOX-DEVELOPMENT
+    message.channel.send(success)
+    db.push('Linkler', { url: Link, owner: message.author.id, owner2: message.author.tag})//PARADOX-DEVELOPMENT
+    db.add(`Sahiplik_${message.author.id}`,1)//PARADOX-DEVELOPMENT
+    db.push(`Projesi_${message.author.id}`,Link)//PARADOX-DEVELOPMENT
     db.add(`Proje`,1)
   }).catch(Hata => {
-  const UpTime = new Discord.MessageEmbed()
+  const dijitaluptime = new Discord.MessageEmbed()
   .setColor('RED')
   .setDescription(`
-  **==================================**
-  **Hata: ${Hata} ❌**
 
-  **Lutfen Bir URL Girin**
-  ==================================
+  **Lütfen Bir URL Girin**
+
   `)
-  .setTimestamp()
-  .setThumbnail(message.author.avatarURL)
-  message.channel.send(UpTime)
+  .setThumbnail(message.author.avatarURL)//PARADOX-DEVELOPMENT
+  message.channel.send(dijitaluptime)//PARADOX-DEVELOPMENT
   })
   }
 
-  if(Split[0] == prefix+'davet') {
-  const Revo = new Discord.RichEmbed()
-  .setColor('#20aaba')
-  .setDescription(`
-  **==================================
-Beni Sunucuna Eklemek Istemen Beni Sevindiriyor Hemen Altta Linkimi Bula Bilirsin Sen Olmassan 1 kisi eksik
-
-[Ekleme Linkim](https://discord.com/api/oauth2/authorize?client_id=782250000901341204&permissions=8&scope=bot)
-
-[Destek Sunucum](https://discord.gg/8JRafVmUqQ)
-
-[Oy Vermeyi Unutma](https://top.gg/bot/782250000901341204/vote)
-==================================
-**`)
-  .setThumbnail(message.author.avatarURL)
-  message.channel.send(Revo)
-  }
+  
 
 
-  if(Split[0] == prefix+'s') {
-  const Revoş = new Discord.MessageEmbed()
-  .setColor('#20aaba')
-  .setThumbnail(message.author.avatarURL)
-  .setTimestamp()
-  .setDescription(`
- 
-**-> Şuanda Toplam \`${db.get('Proje')}\` Botu Uptime Ediyorum. ✅**
 
-`)
-  message.channel.send(Revoş)
-  }
   if(Split[0] == prefix+'say') {
-  const Revoş = new Discord.MessageEmbed()
+  const say = new Discord.MessageEmbed()//PARADOX-DEVELOPMENT
   .setColor('#20aaba')
-  .setThumbnail(message.author.avatarURL)
-  .setTimestamp()
+  .setThumbnail(message.author.avatarURL)//PARADOX-DEVELOPMENT
   .setDescription(`
   
 **-> Şuanda Toplam \`${db.get('Proje')}\` URL Uptime Ediliyor ✅**
 
 **» Bunlardan Sadece \`${db.fetch(`Sahiplik_${message.author.id}`) || null}\` Tanesi Senin ✅**
-==================================`)
-  message.channel.send(Revoş)
+`)
+  message.channel.send(say)
   }
 
-  if(Split[0] == prefix+'yardım') {
+  if(Split[0] == prefix+'yardım') {//PARADOX-DEVELOPMENT
   const pxd = new Discord.MessageEmbed()
-  .setColor('RANDOM')
+  .setColor('RANDOM')//PARADOX-DEVELOPMENT
   .setThumbnail(message.author.avatarURL)
-  .setTimestamp()
+  .setTimestamp()//PARADOX-DEVELOPMENT
   .setAuthor(client.user.username,client.user.avatarURL)
   .setDescription(`
 
-**<a:evet:786584119316447302> » Prefixim: ${prefix}**
 
 `)
   .addField('**» Uptime Bot Komutları**',`
 <a:partner:801075649065910293> » .ekle - Botunuzu Uptime Eder.
 <a:partner:801075649065910293> » .linkler - Uptime ettiğiniz link sayısını gösterir.
-<a:partner:801075649065910293> » .
+<a:partner:801075649065910293> » .say - Tüm Uptime edilmiş link sayısını gösterir.
 `)
   .addField('**Uptime Bot - Hakkında**',`
-
+<a:partner:801075649065910293> » Prefixim: **${prefix}**
 <a:partner:801075649065910293> » [Destek Sunucu](https://discord.gg/gtTTN726aM)
 <a:partner:801075649065910293>  » [Uptime Bot Ekle](https://paradoxphp.com)`)
 
   message.channel.send(pxd)
   }
-
+//PARADOX-DEVELOPMENT
     if(Split[0] == prefix+'linkler') {
     const Linkleri = db.fetch(`Projesi_${message.author.id}`)
     if (!db.get('Linkler').map(Revenge => Revenge.owner).includes(message.author.id)) return message.channel.send(new Discord.MessageEmbed().setColor('#20aaba').setDescription(`**Hiç link eklememişsin. Link Eklemek İçin \`${prefix}ekle\` yazman yeterli**`))
-    message.channel.send(new Discord.MessageEmbed().setColor('#20aaba').setDescription(`**Uptime Etmekte Olduğun Linkler Direkt Mesajlarına Gönderildi . Direkt mesajlarını kontrol et.  ${message.author}**`).setThumbnail(message.author.avatarURL))
+    message.channel.send(new Discord.MessageEmbed().setColor('#20aaba').setDescription(`**Uptime ettiğiniz botlarınızın linklerini güvenlik amaçlı DM yoluyla gönderdik ${message.author}**`).setThumbnail(message.author.avatarURL))
     message.author.send(new Discord.MessageEmbed().setColor('#20aaba').setDescription(`**» Normal Linklerin:** \n\n\``+Linkleri.join('\n')+`\``).setThumbnail(message.author.avatarURL))
     }
 
 
-   
+   //PARADOX-DEVELOPMENT
 })
+//PARADOX-DEVELOPMENT
 
 
-
-
+//PARADOX-DEVELOPMENT
 client.on('ready', () => {
 client.user.setActivity(`${prefix}ekle | ${prefix}yardım`, { type: 'LISTENING' })
 client.user.setStatus('online')
---
+++ oku.md
@@ -0,0 +1,9 @@
+# PARADOX DEVELOPMENT
+
+## 1. Purpose
+
+Altyapı Paradox Developmenta aittir.
+- Discord Sunucumuz]
+
+## 2. Open [Source/Culture/Tech] Citizenship
+
