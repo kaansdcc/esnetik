4f500ff4a96249b6d8d4cbed44992a0d02d2d93f
Glitch (hello-express) <none>
2021-03-14 14:38:35 +0000

    💪🚝 Checkpoint ./bot.js:27587254/2

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on("ready", async () => {
     client.appInfo = await client.fetchApplication();
   }, 600);
 
-client.user.setActivity(`!yardım | 45 Botu Uptime Tutuyorum! `, { type: "PLAYING" });
+client.user.setActivity(`!yardım | 48 Botu Uptime Tutuyorum! `, { type: "PLAYING" });
 
   console.log("`Keevz Bot!");
 });
